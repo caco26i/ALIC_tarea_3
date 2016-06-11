d5f85f3010fdbf3ebaf5334734908af7e4dc7084
Jeffrey Alvarado <jefalva10296@gmail.com>
2016-06-10 20:44:43 -0600

    Agregado Jacobi

--- Final/final.py
+++ Final/final.py
@@ -4,11 +4,11 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 import sys
-#import numpy as np
-#import scipy as sp
-#import scipy.linalg
 import warnings
 import pprint
+import numpy as np
+import scipy as sp
+import scipy.linalg 
 
 class App4:
 	cantidad_vectores_app_1 = None
@@ -115,8 +115,8 @@ class App4:
 		label = self.builder.get_object("label_base_app_2")
 		if isValid:
 			try:
-				P, L, U = lu_decomposition(matriz_elementos)
-				label.set_text("P = \n" + formatMatrix(P) + " \n\nL = \n" + formatMatrix(L) + " \n\nU = \n" + formatMatrix(U))
+				P, L, U = sp.linalg.lu(sp.array(matriz_elementos))
+				label.set_text("P = \n" + str(P) + " \n\nL = \n" + str(L) + " \n\nU = \n" + str(U))
 			except ValueError:
 				label.set_text('No tiene factorizacion LU')
 		app_2.next_page()
@@ -154,9 +154,9 @@ class App4:
 			self.max_iteraciones_app_2 = self.builder.get_object("max_iteraciones").get_text()
 
 			if self.metodo_app_2 is not "" and self.max_error_app_2 is not "" and self.max_iteraciones_app_2 is not "":
-				print self.metodo_app_2
-				print self.max_error_app_2
-				print self.max_iteraciones_app_2
+				print (self.metodo_app_2)
+				print (self.max_error_app_2)
+				print (self.max_iteraciones_app_2)
 				app_3.next_page()
 	
 	def calcular_sistema(self, button):
@@ -190,219 +190,53 @@ class App4:
 		label = self.builder.get_object("label_base_app_2")
 		if isValid:
 			if self.metodo_app_2 == "Jacobi":
-				print "es Jacobi"
-				self.jacobi_reg(matriz_elementos, self.max_error_app_2, self.max_iteraciones_app_2)											
+				try:
+					res = jacobi(np.array(matriz_elementos), np.array(matriz_respuesta), int(self.max_iteraciones_app_2), float(self.max_error_app_2))
+					print (res)
+				except:
+					print("El sistema no tiene solucion")
 			else:
-				print "es Gauss-Seidel"
-
-			print matriz_elementos			
-			print matriz_respuesta
+				print ("es Gauss-Seidel")
 			app_2.next_page()
 
-
-	##### Inicio de la funcion que aplica los metodos	######
-
-	def jacobi_reg(self, mat, Error, MaX): 
-		columnas = 0
-		numIT = 1									
-		switchh = False	
-
-	##### Aplica	metodo Gauss-Jacobi ######
-
-		filas = len(mat)															
-		for C in mat[0]:
-			columnas += 1																 
-
-		print ">> Dimension de la Matriz: [%d][%d]\n" %(filas, columnas)	## Imprime numero de filas x columnas
-		if ((columnas - filas) != 1):
-			print ">> *** Existen mas filas	que columnas ***\n"
-			sys.exit(1)
-
-		###### Primer iteracion ######
-		B = list(zip(*mat)[-1])												## B[] Vector que guarda la ultima columna de la matriz
-		X0 = [B[i]/mat[i][i] for i in xrange(filas)]	## Guarda	resultado de X[i]= b1/a11 , X[i]=b2/a22 etc..
-		X1 = list(X0)																	## X1(actual) = X0(anterior)
-
-		###### Segunda iteracion ######
-		numIT += 1									
-		while not switchh:
-			sup = 0
-			div = 0
-
-			for i in xrange(filas):
-				X1[i] = B[i]
-				for j in xrange(columnas-1):
-					if ( i != j):
-						X1[i]=	(X1[i] - (mat[i][j] * X0[j]))
-
-				X1[i] =	(1/mat[i][i] * X1[i])
-				aux = X1[i] - X0[i]
-				if (aux < 0) :
-					aux = aux * -1
-				aux2 = X1[i]
-				if (aux2 < 0):
-					aux2 = aux2 * -1
-				if (aux > sup):
-					sup = aux
-				if (aux2 > div):
-					div = aux2
-			X0 = list(X1)
-			if (sup / div) <= Error:
-				switchh = True 
-				numIT += 1
-			if int(numIT) > int(MaX):
-				print ">> **Imposible** encontrar resultado en %s *iteraciones*.\n" % MaX	 
-				sys.exit(0)
-	 
-		printM(mat)																			 
-		my_cont = 0
-		print "*** GauSS-JaCoBi ***"
-		for i in X0:																		 ## Imprime	X, resultados de gauss-jacobi
-			print "X%d = %f" % ((my_cont+1), i)
-			my_cont += 1
-		print "\n>> Numero de iteraciones: %d " % numIT
-		print ">> Valor do error: %s" % Error
-
-		####### Fin de Gauss-
-
-		for a in xrange(1, filas):											## Checar a a triangular inferior	
-			for b in xrange(0, columnas):								 
-				if (int(a) != int(b)):
-					if (int(mat[a][b]) != 0):
-						print ">> **Imposible** calcular	triangular inferior * debe ser diferente de 0*\n" 
-						sys.exit(0)
-				elif (int(a) == int(b)):
-						break
-				
-		switchh = False
-		i = filas-1
-		j = i 
-		B[j] = B[j] / mat[i][j]
-		i -= 1
-
-		j = i
-		t = 0
-		numIT = 1
-		columnas -= 1
-
-		while not (switchh):
-			div = 0
-			numIT += 1
-			for t in xrange(j+1, columnas):
-				div = div + (mat[i][t] * B[t])
-			B[j] = (B[j] - div) / mat[i][j]
-			if int(i) == 0:
-				switchh = True
-			i -= 1
-			j = i
-
-		my_cont = 0
-		for i in B:																 ## Imprime B, vetor que guarda resultados retornados
-			print "B%d = %f" % ((my_cont+1), i)
-			my_cont += 1 
-	
 	
 warnings.filterwarnings('error')
 
-def mult_matrix(M, N):
-																																																		 
-	tuple_N = zip(*N)
+###Jacobi
 
-	# comprension de la lista anidada para calcular el producto de matrices																																													 
-	return [[sum(el_m * el_n for el_m, el_n in zip(row_m, col_n)) for col_n in tuple_N] for row_m in M]
-
-def pivot_matrix(M):
-	"""Returna el pivote de la matriz	M"""
-	m = len(M)
-
-	# Crear una	matriz, identica con	valores flotantes																																															
-	id_mat = [[float(i ==j) for i in range(m)] for j in range(m)]
-
-	# Reordenar la matriz identidad de manera que el elemento mas grande de cada columna
-	# de M se coloque en la diagonal de M																																															
-	for j in range(m):
-		row = max(range(j, m), key=lambda i: abs(M[i][j]))
-		if j != row:
-			# intercambiar filas																																																							
-			id_mat[j], id_mat[row] = id_mat[row], id_mat[j]
-
-	return id_mat
-
-def lu_decomposition(A):
-	"""Hacer Decomposicion LU de A (debe ser cuadrada)																																														
-	 PA = LU. la funcion retorna P, L y U."""
-	n = len(A)
-
-	# crear matrices nulas para L y U																																																				 
-	L = [[0.0] * n for i in range(n)]
-	U = [[0.0] * n for i in range(n)]
-
-	# Crear el pivote de la matriz P y el producto de PA																																														 
-	P = pivot_matrix(A)
-	PA = mult_matrix(P, A)
-
-	# Descomposicion	LU																																																					
-	for j in range(n):
-		# todas las	diagonales de L son unidad																																																	 
-		L[j][j] = 1.0
-
-		for i in range(j+1):
-			s1 = sum(U[k][j] * L[i][k] for k in range(i))
-			U[i][j] = PA[i][j] - s1
-																																								
-		for i in range(j, n):
-			s2 = sum(U[k][j] * L[i][k] for k in range(j))
-			L[i][j] = (PA[i][j] - s2) / U[j][j]
-
-	return (P, L, U)
+def jacobi(A, b, iterationLimit, error):
 	
-def formatMatrix(pMatrix):
 	resultado = ""
-	for i in range (len(pMatrix)):
-		for j in range (len(pMatrix[0])):
-			resultado += str(pMatrix[i][j])
-			resultado += "\t"
-		resultado += "\n"
+
+	# prints the system
+	resultado += "System:\n"
+	for i in range(A.shape[0]):
+			row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
+			resultado += " + " + str(row) + "=" + str(b[i]) + "\n"
+
+	x = np.zeros_like(b)
+	for it_count in range(iterationLimit):
+			resultado += "\n\nSolucion actual:" + str(x) + "\n"
+			x_new = np.zeros_like(x)
+
+			for i in range(A.shape[0]):
+					s1 = np.dot(A[i, :i], x[:i])
+					s2 = np.dot(A[i, i + 1:], x[i + 1:])
+					x_new[i] = (b[i] - s1 - s2) / A[i, i]
+
+			if np.allclose(x, x_new, atol=error):
+					break
+
+			x = x_new
+
+			error = np.dot(A, x) - b
+			resultado += "Error:" + str(error) + "\n"
+
+	resultado += "\n\nSolucion final:" + str(x) + "\n"
+
 	return resultado
+
 
 if __name__ == "__main__":
 	main = App4()
 	Gtk.main()
-
-
-############# JACOBI
-
-
-class InvalidSizeError(Exception):
-	"""Excepcion en caso que las matrices tengan un tamnio no adecuado"""
-	pass
-
-def printM(m):																 
-	
-	for j in m:
-		for elem in j:
-			print "%3d" % elem, 
-		print ""
-	print ""
-
-def readM():																	 
-	
-	 matriz = []																 
-	 lastsize = 0																## Guarda	tamanio de la ultima fila leida 
-																							 
-
-	 print ">> Ingrese MATRIZ:	(fila	en blanco para terminar):"
-	 while 1:
-			r = sys.stdin.readline()								 
-			if r.strip():														
-				l = r.split()												 
-																							 ##.. '1 2	 32 1 2' --> ['1', '2', '32', '1', '2']
-				s = len(l)														 
-				if lastsize and	s != lastsize:				
-					raise InvalidSizeError, "As linhas devem ter todas o mesmo numero de colunas." 
-				matriz.append([float(elem) for elem in l]) ## Convierte los elementos de la fila a enteros
-																								 ##.. e coloca la matriz
-				lastsize = s													 
-			else:
-				break										 
-	 return matriz									
